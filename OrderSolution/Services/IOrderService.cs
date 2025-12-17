71e67ad94eb143ea3fc1976397c22885290a41c7
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-17 11:13:01 +0200

    added a service folder with an interface class and normal class

---
+++ OrderSolution/Services/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using OrderSolution.Models;
+
+namespace OrderSolution.Services
+{
+    public interface IOrderService
+    {
+        IEnumerable<Order> GetAll();
+        Order? GetById(Guid id);
+        Order Create(Order order);
+        bool UpdateStatus(Guid id, string status);
+    }
+}
---
+++ OrderSolution/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using OrderSolution.Models;
+
+namespace OrderSolution.Services
+{
+    public class OrderService : IOrderService
+    {
+        private static readonly List<Order> _orders = new();
+
+
+        public IEnumerable<Order> GetAll() => _orders;
+
+
+        public Order? GetById(Guid id) => _orders.FirstOrDefault(o => o.Id == id);
+
+
+        public Order Create(Order order)
+        {
+            order.Id = Guid.NewGuid();
+            _orders.Add(order);
+            return order;
+        }
+
+
+        public bool UpdateStatus(Guid id, string status)
+        {
+            var order = GetById(id);
+            if (order == null) return false;
+
+
+            order.Status = status;
+            return true;
+        }
+    }
+}
