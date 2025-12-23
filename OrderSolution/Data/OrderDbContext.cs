22ae21bf3b52bab895d5ff81a4a54eeb27ea4695
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-23 18:03:28 +0200

    added db context

---
+++ OrderSolution/Data/OrderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderSolution.Models;
+
+namespace OrderSolution.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+            : base(options) { }
+
+        public DbSet<Order> Orders => Set<Order>();
+    }
+}
